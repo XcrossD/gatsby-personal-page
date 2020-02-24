9cbec48c7ead119c3e739e0504afdd0f036d3ff9
Herbert Lin <herbert.lin.7@gmail.com>
2020-02-24 23:08:54 +0800

    Install and import react-bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
