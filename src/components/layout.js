d1e714d3891234743612ebd45a302333f1677c5f
Herbert Lin <herbert.lin.7@gmail.com>
2020-02-24 23:22:27 +0800

    Create social media

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
   render() {
+    library.add(fab);
+
     const { children, title } = this.props;
     return (
       <React.Fragment>
@@ -37,8 +42,31 @@ class Layout extends React.Component {
               <h3>Your name</h3>
               <a href="mailto:yourname@somemail.com">yourname@somemail.com</a>
             </Col>
-            <Col md={4}>
-              // 這裡做 social media
+            <Col md={4} className="d-flex align-items-center justify-content-end">
+              <div className="social-media">
+                <ul>
+                  <li>
+                    <a href="https://github.com/yourgithub">
+                      <FontAwesomeIcon icon={['fab', 'github']} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/yourfacebook">
+                      <FontAwesomeIcon icon={['fab', 'facebook']} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/yourinstagram/">
+                      <FontAwesomeIcon icon={['fab', 'instagram']} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/yourlinkedin/">
+                      <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </Col>
           </Row>
         </Container>
