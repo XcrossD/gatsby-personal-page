8596be170d5a1075aff7b8b08f1f16bbf2efbd40
Herbert Lin <herbert.lin.7@gmail.com>
2020-02-24 23:32:03 +0800

    Complete layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
+const FooterWrapper = styled.footer`
+  background-color: #171717;
+  color: #f1f1f1;
+  margin-top: 100px;
+  padding: 50px 0;
+`;
+
+const EmailLink = styled.a`
+  text-decoration: none;
+  background-color: transparent;
+  color: #f1f1f1;
+  &:hover, &:visited, &:active {
+    color: #f1f1f1;
+  }
+`;
+
+const SocialMedia = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: flex-end;
+  align-content: stretch;
+  text-align: right;
+`;
+
+const SocialMediaUl = styled.ul`
+  list-style: none;
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+`;
+
+const SocialMediaLi = styled.li`
+  display: inline-block;
+  margin: 0 18px 0 18px;
+  padding: 0;
+`;
+
+const SocialMediaLink = styled.a`
+  text-decoration: none;
+  background-color: transparent;
+  color: #f1f1f1;
+  &:hover, &:visited, &:active {
+    color: #f1f1f1;
+  }
+`;
+
 class Layout extends React.Component {
   render() {
     library.add(fab);
@@ -18,7 +65,7 @@ class Layout extends React.Component {
     return (
       <React.Fragment>
         <Container>
-          <Navbar>
+          <Navbar className="mb-3 pl-0">
             <Navbar.Brand as={Link} to="/">Your name</Navbar.Brand>
             <Nav className="ml-auto">
               <Nav.Item>
@@ -34,40 +81,42 @@ class Layout extends React.Component {
           </Navbar>
           {children}
         </Container>
-        <Container>
-          <Row>
-            <Col md={8}>
-              <h3>Your name</h3>
-              <a href="mailto:yourname@somemail.com">yourname@somemail.com</a>
-            </Col>
-            <Col md={4} className="d-flex align-items-center justify-content-end">
-              <div className="social-media">
-                <ul>
-                  <li>
-                    <a href="https://github.com/yourgithub">
-                      <FontAwesomeIcon icon={['fab', 'github']} />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.facebook.com/yourfacebook">
-                      <FontAwesomeIcon icon={['fab', 'facebook']} />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.instagram.com/yourinstagram/">
-                      <FontAwesomeIcon icon={['fab', 'instagram']} />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.linkedin.com/in/yourlinkedin/">
-                      <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        <FooterWrapper>
+          <Container className="pt-6">
+            <Row>
+              <Col md={8} className="d-flex flex-column justify-content-center">
+                <h3>Your name</h3>
+                <EmailLink href="mailto:yourname@somemail.com">yourname@somemail.com</EmailLink>
+              </Col>
+              <Col md={4} className="d-flex align-items-center justify-content-end">
+                <SocialMedia className="social-media">
+                  <SocialMediaUl>
+                    <SocialMediaLi>
+                      <SocialMediaLink href="https://github.com/yourgithub">
+                        <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
+                      </SocialMediaLink>
+                    </SocialMediaLi>
+                    <SocialMediaLi>
+                      <SocialMediaLink href="https://www.facebook.com/yourfacebook">
+                        <FontAwesomeIcon icon={['fab', 'facebook']} size="lg" />
+                      </SocialMediaLink>
+                    </SocialMediaLi>
+                    <SocialMediaLi>
+                      <SocialMediaLink href="https://www.instagram.com/yourinstagram/">
+                        <FontAwesomeIcon icon={['fab', 'instagram']} size="lg" />
+                      </SocialMediaLink>
+                    </SocialMediaLi>
+                    <SocialMediaLi>
+                      <SocialMediaLink href="https://www.linkedin.com/in/yourlinkedin/">
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} size="lg" />
+                      </SocialMediaLink>
+                    </SocialMediaLi>
+                  </SocialMediaUl>
+                </SocialMedia>
+              </Col>
+            </Row>
+          </Container>
+        </FooterWrapper>
       </React.Fragment>
     )
   }
