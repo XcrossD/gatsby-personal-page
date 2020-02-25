a5c9eb9252f44e1221cff15ac222ec355765ab78
Herbert Lin <herbert.lin.7@gmail.com>
2020-02-25 14:34:28 +0800

    Change blog page into bootstrap style

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,35 +1,31 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/button"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 class Blog extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMdx.edges
+    const { data } = this.props;
+    const posts = data.allMdx.edges;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout>
         <SEO title="All posts" />
-        <Bio />
-        <div style={{ margin: "20px 0 40px" }}>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <h3
-                  style={{
-                    marginBottom: `10px`,
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={`blog${node.fields.slug}`}
-                  >
+        <Row className="mb-4">
+          <Col>
+            <h3>Blog</h3>
+          </Col>
+        </Row>
+        {posts.map(({ node }) => {
+           const title = node.frontmatter.title || node.fields.slug
+           return (
+             <Row key={node.fields.slug}>
+               <Col>
+                <h3>
+                  <Link to={`blog${node.fields.slug}`}>
                     {title}
                   </Link>
                 </h3>
@@ -39,13 +35,10 @@ class Blog extends React.Component {
                     __html: node.frontmatter.description || node.excerpt,
                   }}
                 />
-              </div>
-            )
-          })}
-        </div>
-        <Link to="/">
-          <Button marginTop="85px">Go Home</Button>
-        </Link>
+               </Col>
+             </Row>
+           )
+        })}
       </Layout>
     )
   }
@@ -55,11 +48,6 @@ export default Blog
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
